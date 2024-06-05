139704c31f2ee1361a49293d8d048554bb15026d
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-06-05 13:30:26 +0530

    banner, navbar and app complete with styles also

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
+import './App.css'
 
 function App() {
   return (
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -9,9 +9,10 @@ function Banner() {
         <div className="banner_buttons">
             <button className="button">Play</button>
             <button className="button">My list</button>
-            <h1 className="description">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</h1>
         </div>
+            <h1 className="description">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</h1>
       </div>
+      <div className="fade_bottom"></div>
     </div>
   )
 }
